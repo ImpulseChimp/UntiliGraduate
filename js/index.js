a83e8ff201c8353a69b16f3c47aa9aa415cdd61f
Christopher M. Wood <cmw2379@vt.edu>
2015-09-15 10:19:01 -0400

    Basic working version of site

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
+var displayText = [
+    "am free",
+    "can sleep",
+    "have free time",
+    "can relax",
+    "can sleep in",
+    "never have tests",
+    "can make money",
+    "can enjoy weekends",
+    "can start life"
+]
+var textObject = $("#until-text");
+var displaySeconds = 10000;
 var clock;
 
 $(document).ready(function() {
+
+    textObject.textillate({ in: { loop: true, effect: 'rollIn' } });
+
 	var clock;
 
     var now = new Date();
@@ -11,5 +27,21 @@ $(document).ready(function() {
 	clock = $('#countdown-clock').FlipClock(secondsTillGraduation, {
         clockFace: 'DailyCounter'
     });
+
+    setInterval(function () {
+        var newText = getNewDisplayText();
+        swapUntilText(newText);
+    }, displaySeconds);
 		   
 });
+
+function swapUntilText(text) {
+    textObject.unbind(); //Does not work yet
+    textObject.textillate({ in: { loop: true, effect: 'rollIn' } }); //Does not work yet
+    textObject.text(text);
+}
+
+function getNewDisplayText() {
+    var randomVal = Math.floor(Math.random() * displayText.length);
+    return displayText[randomVal];
+}
