13485b60608c0d11c3f0c5626480872a570767f4
Christopher M. Wood <cmw2379@vt.edu>
2015-10-16 20:34:42 -0400

    custom graduation date added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
+var displayText = [
+    "am free",
+    "can sleep",
+    "have free time",
+    "can relax",
+    "can sleep in",
+    "never have tests",
+    "can make money",
+    "can enjoy weekends",
+    "can start life"
+]
+var textObject = $("#until-text");
+var displaySeconds = 10000;
+var clock;
+
 
 setCookieValuesIfPossible();
-
 $(function() {
 
     var popupWidth = 600;
@@ -25,59 +39,9 @@ $(function() {
         onUnload: function() { settingsPopup.hide(); }
     });
 
-    $('#countdown-date-input').pickadate();
+    $('#custom-grad-date-input').pickadate();
     $('.tiltshift').tiltShift();
 
-});
-
-function setCookieValuesIfPossible() {
-    var countdown_date = getCookie("uig_countdown_date");
-    var custom_background = getCookie("uig_custom_background");
-
-    if(custom_background.length > 0) {
-        setSiteBackground(custom_background);
-        $("#custom-background-input").val(custom_background)
-    }
-    else {
-        setSiteBackground("assets/index_background.jpg");
-    }
-
-    if(countdown_date.length > 0) {
-        setSiteCountdownTimer(custom_background);
-        $("#countdown-date-input").val(countdown_date)
-    }
-}
-
-$("#save-settings").on("click", function() {
-    setCookie("uig_custom_background", $("#custom-background-input").val(), 10000);
-    setCookie("uig_countdown_date", $("#countdown-date-input").val(), 10000);
-    setCookieValuesIfPossible();
-});
-
-function setSiteBackground(background_url) {
-    $('#page-container').css('background-image', 'url("' + background_url + '")');
-}
-
-function setSiteCountdownTimer(custom_time) {
-
-}
-
-var displayText = [
-    "am free",
-    "can sleep",
-    "have free time",
-    "can relax",
-    "can sleep in",
-    "never have tests",
-    "can make money",
-    "can enjoy weekends",
-    "can start life"
-]
-var textObject = $("#until-text");
-var displaySeconds = 10000;
-var clock;
-
-$(document).ready(function() {
 
     textObject.textillate({ in: { loop: true, effect: 'rollIn' } });
 
@@ -97,8 +61,57 @@ $(document).ready(function() {
         var newText = getNewDisplayText();
         swapUntilText(newText);
     }, displaySeconds);
-           
+      
 });
+
+function setCookieValuesIfPossible() {
+    var countdown_date = getCookie("uig_countdown_date");
+    var custom_background = getCookie("uig_custom_background");
+    var year = getCookie("uig_custom_year");
+    var month = getCookie("uig_custom_month");
+    var day = getCookie("uig_custom_day");
+
+    if(custom_background.length > 0) {
+        setSiteBackground(custom_background);
+        $("#custom-background-input").val(custom_background)
+    }
+    else {
+        setSiteBackground("assets/index_background.jpg");
+    }
+
+    if(countdown_date.length > 0) {
+        setCountdownClock(year, month, day);
+        $("#countdown-date-input").val(countdown_date)
+    }
+}
+
+$("#save-settings").on("click", function() {
+    var time = $('#custom-grad-date-input').pickadate('get', 'highlight');
+    
+    setCookie("uig_custom_background", $("#custom-background-input").val(), 10000000);
+    setCookie("uig_countdown_date", $("#countdown-date-input").val(), 10000000);
+    setCookie("uig_countdown_day", time.year, 10000000);
+    setCookie("uig_countdown_month", time.month, 10000000);
+    setCookie("uig_countdown_year", time.day, 10000000);
+
+    setCookieValuesIfPossible();
+});
+
+function setSiteBackground(background_url) {
+    $('#page-container').css('background-image', 'url("' + background_url + '")');
+}
+
+function setCountdownClock(year, month, day) {
+    var now = new Date();
+    var graduation = new Date(year, month, day, 0, 0, 0);
+    var msTillGraduation = graduation.getTime() - now.getTime();
+    var secondsTillGraduation = msTillGraduation / 1000;
+
+    clock = $('#countdown-clock').FlipClock(secondsTillGraduation, {
+        clockFace: 'DailyCounter',
+        countdown: true
+    });
+}
 
 function swapUntilText(text) {
     textObject.unbind(); //Does not work yet
